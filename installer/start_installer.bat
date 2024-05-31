81d5b88c2f96ebf7d89831a975984a03327db669
Xindong Su <suxindong@sina.com>
2024-06-01 00:09:39 +0800

    add flac commandline tools to project. add project solution file. remove unused installer files.

--- installer/start_installer.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-"C:\Program Files\WiX Toolset v3.7\bin\candle.exe" "FLAC frontend.wxs"
-"C:\Program Files\WiX Toolset v3.7\bin\light.exe" -ext WixUIExtension -ext WixNetFxExtension "FLAC frontend.wixobj"
-PAUSE
